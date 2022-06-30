8336e12ac33628f211cc9561f6abeae1cb87b788
Lucas Abe <lucasabe95@gmail.com>
2022-06-30 20:45:22 -0300

    Adjusting initial list of heroes + rework on folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MainService } from 'src/service/main.service';
 
 @Component({
   selector: 'app-root',
@@ -8,34 +7,13 @@ import { MainService } from 'src/service/main.service';
 })
 export class AppComponent {
   title = 'new-study';
-  characters = []
-  offset = 0
+  
   constructor(
-    private mainService: MainService
   ) {
 
   }
   
   ngOnInit() {
-    this.getCharacters()
   }
 
-  async getCharacters() {
-    this.mainService.getCharacters().subscribe(data => 
-      this.characters = data.data.results
-    )
-  }
-  async backPage() {
-    this.offset = this.offset - 20
-    this.mainService.getOffsetCharacters(this.offset).subscribe(data => {
-      this.characters = data.data.results
-    })
-  }
-
-  async nextPage() {
-    this.offset = this.offset + 20
-    this.mainService.getOffsetCharacters(this.offset).subscribe(data => {
-      this.characters = data.data.results
-    })
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
-import { AppHeaderComponent } from './app-header/app-header.component';
-import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppHeaderComponent } from './layout/app-header/app-header.component';
+import { AppFooterComponent } from './layout/app-footer/app-footer.component';
+import { PageTemplateComponent } from './layout/page-template/page-template.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CharacterCardComponent } from './character-card/character-card.component';
+import { CharacterCardComponent } from './components/character-card/character-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
     AppFooterComponent,
-    CharacterCardComponent
+    CharacterCardComponent,
+    PageTemplateComponent,
   ],
   imports: [
     AppRoutingModule,
