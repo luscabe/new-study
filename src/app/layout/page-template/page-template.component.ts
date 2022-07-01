fd2c8e32ae239b06cb0ff708748a78f57865a754
Lucas Abe <lucasabe95@gmail.com>
2022-07-01 15:53:06 -0300

    Removing unecessary methods, ajusting colors and some css

--- src/app/layout/page-template/page-template.component.ts
+++ src/app/layout/page-template/page-template.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MainService } from 'src/app/services/main.service';
 
 @Component({
   selector: 'page-template',
@@ -7,35 +6,12 @@ import { MainService } from 'src/app/services/main.service';
   styleUrls: ['./page-template.component.sass']
 })
 export class PageTemplateComponent implements OnInit {
-  characters = []
-  offset = 0
   constructor(
-    private mainService: MainService
   ) {
     
   }
 
   ngOnInit() {
-    this.getCharacters()
-  }
-
-  async getCharacters() {
-    this.mainService.getCharacters().subscribe(data => 
-      this.characters = data.data.results
-    )
-  }
-  async backPage() {
-    this.offset = this.offset - 20
-    this.mainService.getOffsetCharacters(this.offset).subscribe(data => {
-      this.characters = data.data.results
-    })
-  }
-
-  async nextPage() {
-    this.offset = this.offset + 20
-    this.mainService.getOffsetCharacters(this.offset).subscribe(data => {
-      this.characters = data.data.results
-    })
   }
 
 }
